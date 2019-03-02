bcee44b40068ffb930853dacc716a7cb1e9210bf
huyingzhe <44468993+huyingzhe@users.noreply.github.com>
2019-03-02 10:59:24 +0800

    Create MyGreeter.php

---
+++ php/test/MyGreeter.php
@@ -0,0 +1,10 @@
+class Client {
+    function getGreeting(){
+        $h=date('G');
+        if ($h<11) echo '早上好';
+        else if ($h<13) echo '中午好';
+        else if ($h<17) echo '下午好';
+        else echo '晚上好';
+    }
+
+}
